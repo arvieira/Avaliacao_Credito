97dec51273fab0b7b69d37e01acd2a97c2785fb8
arvieira <andreribeirovieira@gmail.com>
2021-08-23 15:19:57 -0300

    Acrescentando ao pycharm e escondendo o menu do canto superior direito

--- utils.py
+++ utils.py
@@ -7,27 +7,27 @@ import pandas as pd
 
 
 class Transformador(BaseEstimator, TransformerMixin):
-  # Construtor
-  def __init__(self, colunas_continuas, colunas_categoricas):
-    self.colunas_continuas = colunas_continuas
-    self.colunas_categoricas = colunas_categoricas
-    self.enc = OneHotEncoder()
-    self.scaler = MinMaxScaler()
+    # Construtor
+    def __init__(self, colunas_continuas, colunas_categoricas):
+        self.colunas_continuas = colunas_continuas
+        self.colunas_categoricas = colunas_categoricas
+        self.enc = OneHotEncoder()
+        self.scaler = MinMaxScaler()
 
-  # Treinamento
-  def fit(self, X, y=None):
-    self.enc.fit(X[self.colunas_categoricas])
-    self.scaler.fit(X[self.colunas_continuas])
-    return self
+    # Treinamento
+    def fit(self, X, y=None):
+        self.enc.fit(X[self.colunas_categoricas])
+        self.scaler.fit(X[self.colunas_continuas])
+        return self
 
-  # Método que vai efetivar a transformação
-  def transform(self, X, y=None):
-    X_categoricas = pd.DataFrame(data=self.enc.transform(X[self.colunas_categoricas]).toarray(),
-                                 columns=self.enc.get_feature_names(self.colunas_categoricas))
-    
-    X_continuas = pd.DataFrame(data=self.scaler.transform(X[self.colunas_continuas]),
-                               columns=self.colunas_continuas)
-    
-    X = pd.concat([X_continuas, X_categoricas], axis=1)
+    # Método que vai efetivar a transformação
+    def transform(self, X, y=None):
+        X_categoricas = pd.DataFrame(data=self.enc.transform(X[self.colunas_categoricas]).toarray(),
+                                     columns=self.enc.get_feature_names(self.colunas_categoricas))
 
-    return X
+        X_continuas = pd.DataFrame(data=self.scaler.transform(X[self.colunas_continuas]),
+                                   columns=self.colunas_continuas)
+
+        X = pd.concat([X_continuas, X_categoricas], axis=1)
+
+        return X
