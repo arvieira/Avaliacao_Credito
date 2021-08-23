c51a87973e243da28509206f28726468c84c13c6
arvieira <andreribeirovieira@gmail.com>
2021-08-23 15:15:22 -0300

    Acrescentando ao pycharm e escondendo o menu do canto superior direito

--- simulador_avaliacao_credito.py
+++ simulador_avaliacao_credito.py
@@ -3,7 +3,6 @@ from joblib import load
 import pandas as pd
 from utils import Transformador
 
-
 # As linhas #MainMenu e footer servem para esconder o menu do canto superior direito da página
 # A última linha foi da aula e serve para mudar a cor do background dos combobox
 hide_streamlit_style = """
@@ -67,7 +66,6 @@ def avaliar_mau(dict_respostas):
     return mau
 
 
-
 # Escrevendo uma imagem e um título
 st.image('img/bytebank_logo.png')
 st.write('# Simulador de Avaliação de crédito')
@@ -91,42 +89,57 @@ with my_expander_1:
 
     # Na coluna 1 estou colocando um combobox com as opções importadas do notebook
     # A seleção do usuário é colocada no retorno do combobox
-    dict_respostas['Categoria_de_renda'] = col1_form.selectbox('Qual a categoria de renda ?', lista_campos['Categoria_de_renda'])
+    dict_respostas['Categoria_de_renda'] = col1_form.selectbox('Qual a categoria de renda ?',
+                                                               lista_campos['Categoria_de_renda'])
 
     dict_respostas['Ocupacao'] = col1_form.selectbox('Qual a ocupação ?', lista_campos['Ocupacao'])
 
-     # Definindo uma variável categórica binária de sim e não mapeada em 1 e 0
-    dict_respostas['Tem_telefone_trabalho'] = 1 if col1_form.selectbox('Tem um telefone do trabalho ?', ['Sim', 'Não']) == 'Sim' else 0
-
+    # Definindo uma variável categórica binária de sim e não mapeada em 1 e 0
+    dict_respostas['Tem_telefone_trabalho'] = 1 if col1_form.selectbox('Tem um telefone do trabalho ?',
+                                                                       ['Sim', 'Não']) == 'Sim' else 0
 
     # Slider é uma barra horizontal para selecionar um valor
     # No dataframe o salário era anual. Aqui pedimos o mensal que é mais comum para o usuário e multiplicamos por 12
-    dict_respostas['Rendimento_Anual'] = col2_form.slider('Qual o salário mensal ?', help='Podemos mover a barra usando as setas do teclado', min_value=0, max_value=35000, step=500) * 12
-    
+    dict_respostas['Rendimento_Anual'] = col2_form.slider('Qual o salário mensal ?',
+                                                          help='Podemos mover a barra usando as setas do teclado',
+                                                          min_value=0, max_value=35000, step=500) * 12
+
     # Observe que o modelo que criamos utilizava anos empregado como positivo e desempregado como negativo. 
     # Nesse caso, teremos que posteriormente tratar esses campos para submeter ao modelo.
-    dict_respostas['Anos_empregado'] = col2_form.slider('Quantos anos empregado ?', help='Podemos mover a barra usando as setas do teclado', min_value=0, max_value=50, step=1)
-    dict_respostas['Anos_desempregado'] = col2_form.slider('Quantos anos desempregado ?', help='Podemos mover a barra usando as setas do teclado', min_value=0, max_value=50, step=1)
-    
+    dict_respostas['Anos_empregado'] = col2_form.slider('Quantos anos empregado ?',
+                                                        help='Podemos mover a barra usando as setas do teclado',
+                                                        min_value=0, max_value=50, step=1)
+    dict_respostas['Anos_desempregado'] = col2_form.slider('Quantos anos desempregado ?',
+                                                           help='Podemos mover a barra usando as setas do teclado',
+                                                           min_value=0, max_value=50, step=1)
+
 with my_expander_2:
     col3_form, col4_form = st.beta_columns(2)
 
-    dict_respostas['Grau_Escolaridade'] = col3_form.selectbox('Qual o Grau de Escolaridade ?', lista_campos['Grau_Escolaridade'])
+    dict_respostas['Grau_Escolaridade'] = col3_form.selectbox('Qual o Grau de Escolaridade ?',
+                                                              lista_campos['Grau_Escolaridade'])
     dict_respostas['Estado_Civil'] = col3_form.selectbox('Qual o Estado Civil ?', lista_campos['Estado_Civil'])
     dict_respostas['Tem_Carro'] = 1 if col3_form.selectbox('Tem um Carro ?', ['Sim', 'Não']) == 'Sim' else 0
-    
-    dict_respostas['Tem_telefone_fixo'] = 1 if col4_form.selectbox('Tem um telefone fixo ?', ['Sim', 'Não']) == 'Sim' else 0
+
+    dict_respostas['Tem_telefone_fixo'] = 1 if col4_form.selectbox('Tem um telefone fixo ?',
+                                                                   ['Sim', 'Não']) == 'Sim' else 0
     dict_respostas['Tem_email'] = 1 if col4_form.selectbox('Tem um email ?', ['Sim', 'Não']) == 'Sim' else 0
-    dict_respostas['Idade'] = col4_form.slider('Qual a idade ?', help='Podemos mover a barra usando as setas do teclado', min_value=0, max_value=100, step=1)
+    dict_respostas['Idade'] = col4_form.slider('Qual a idade ?',
+                                               help='Podemos mover a barra usando as setas do teclado', min_value=0,
+                                               max_value=100, step=1)
 
 with my_expander_3:
     col5_form, col6_form = st.beta_columns(2)
 
     dict_respostas['Moradia'] = col5_form.selectbox('Qual o tipo de moradia ?', lista_campos['Moradia'])
     dict_respostas['Tem_Casa_Propria'] = 1 if col5_form.selectbox('Tem Casa Propria ?', ['Sim', 'Não']) == 'Sim' else 0
-    
-    dict_respostas['Tamanho_Familia'] = col6_form.slider('Qual o tamaho da família ?', help='Podemos mover a barra usando as setas do teclado', min_value=1, max_value=20, step=1)
-    dict_respostas['Qtd_Filhos'] = col6_form.slider('Quantos filhos ?', help='Podemos mover a barra usando as setas do teclado', min_value=0, max_value=20, step=1)
+
+    dict_respostas['Tamanho_Familia'] = col6_form.slider('Qual o tamaho da família ?',
+                                                         help='Podemos mover a barra usando as setas do teclado',
+                                                         min_value=1, max_value=20, step=1)
+    dict_respostas['Qtd_Filhos'] = col6_form.slider('Quantos filhos ?',
+                                                    help='Podemos mover a barra usando as setas do teclado',
+                                                    min_value=0, max_value=20, step=1)
 
 # O button retorna dois valores True ou False. Retorna True se estiver pressionado
 # e false caso não esteja. Por isso, eu posso colocar o button no if
